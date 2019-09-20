1270d7cf79bac056b9ce54e661b79ebe3ba2470e
unknown <cobsmr@gmail.com>
2019-09-20 14:36:29 +0700

    Updates

---
+++ precache-manifest.59d437b831720c13e8525ca610bdf786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bc85c3d13444a21b7f20d9b021d167",
+    "url": "/react-monsterRolodex/index.html"
+  },
+  {
+    "revision": "a23eafba8e6d40b50dcb",
+    "url": "/react-monsterRolodex/static/css/main.a1deb804.chunk.css"
+  },
+  {
+    "revision": "0792b5e2043d95efda44",
+    "url": "/react-monsterRolodex/static/js/2.1159b1ef.chunk.js"
+  },
+  {
+    "revision": "a23eafba8e6d40b50dcb",
+    "url": "/react-monsterRolodex/static/js/main.06320549.chunk.js"
+  },
+  {
+    "revision": "c38dd6521b113abf86c0",
+    "url": "/react-monsterRolodex/static/js/runtime-main.f1c513cf.js"
+  }
+]);
